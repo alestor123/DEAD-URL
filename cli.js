69e455018403038bdd389fb0e88498c2085c32c2
alestor123 <alestoraldous@gmail.com>
2021-06-07 21:28:40 +0530

    added main cli

--- cli.js
+++ cli.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+const dead = require('./App')
+const chalk = require('chalk')
+
+dead('./lol.md').forEach(element =>
+  element.urls.forEach(async lol => {
+    const shey = (await lol)
+    if (!shey.stats) console.log(chalk.redBright.bold(shey.url + ' : IS DEAD'))
+  })
+)
